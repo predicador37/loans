b36cd5f36470b89b9f4c36d20c898abded7a6f0a
predicador37 <predicador@kaitain.dune.com>
2013-11-12 06:49:08 +0100

    some changes

--- datamunging.R
+++ datamunging.R
@@ -4,5 +4,11 @@ download.file(fileUrl, destfile="./data/loansData.csv", method="curl")
 loansData = read.csv("./data/loansData.csv")
 loansData$Interest.Rate <- as.numeric(gsub( "%", "", as.character(loansData$Interest.Rate)))
 loansData$Debt.To.Income.Ratio <- as.numeric(gsub( "%", "", as.character(loansData$Debt.To.Income.Ratio)))
-loansDataByFICO <- split(loansData, loansData$FICO.Range)
+loansData$FICO.Range <- as.numeric(lapply(strsplit(as.character(loansData$FICO.Range),"-"),"[",1))   
+loansData$cluster  <- kmeans(loansData[,10], centers=3)$cluster
+loansDataByFICO <- split(loansData, loansData$cluster)
+loansData1=loansDataByFICO[[1]]
+loansData2=loansDataByFICO[[2]]
+loansData3=loansDataByFICO[[3]]
+lm <- lm(loansData1$Interest.Rate~loansData1$Interest.Rate)
 }
--- plots.R
+++ plots.R
@@ -2,3 +2,9 @@ pairs(~Amount.Requested+Amount.Funded.By.Investors+Interest.Rate+Monthly.Income+
 chart.Correlation(loansData[,c(1,2,3,9,11,12,13)], histogram=TRUE, pch="+")
 plot(loansDataByFICO[[10]][,]), loansDataByFICO[[10]][,])
 qplot(loansDataByFICO[[10]][,1], loansDataByFICO[[10]][,3], geom=c("point","smooth"))
+
+dev.new(width=4, height=4)
+with(loansData, plot(FICO.Range, Interest.Rate, type='n'))
+cor.test(loansData$Interest.Rate[loansData$FICO.Range>680 & loansData$FICO<725],loansData$Amount.Requested[loansData$FICO.Range>680 & loansData$FICO<725])
+
+#with(loansData, text(var1, var2, var3, col=cl))
